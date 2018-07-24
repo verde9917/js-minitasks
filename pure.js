619a7cfa06e084ed763cae7e0d29821ce2235c72
n-nakajm <n-nakajm@ist.osaka-u.ac.jp>
2018-07-24 20:50:58 +0900

    make task05

--- pure.js
+++ pure.js
@@ -1,27 +1,34 @@
-// task01
+// task no01
 changeTextColor = () => {
     let text = document.getElementById('targetOfChangeColor');
     text.style.color = '#ff95c8';
 }
 document.getElementById('TextColor').addEventListener('click', changeTextColor);
 
-// task02
+// task no02
 changeText = () => {
     let text = document.getElementById('targetOfChangeText');
     text.innerHTML = 'Hello,JavaScript!';
 }
 document.getElementById('firstText').addEventListener('click', changeText);
 
-// task03
+// task no03
 showTypedText = () => {
     let typed = document.getElementById('typeText').value;
     document.getElementById('typedText').innerHTML = typed;
 }
 document.getElementById('typeText').addEventListener('keyup',showTypedText);
 
-// task04
-addDifferentClass = () => {
+// task no04
+addClass = () => {
     let targetDiv = document.getElementById('setClass');
     targetDiv.classList.add('newClass');
 }
-document.getElementById('setClass').addEventListener('click',addDifferentClass);
+document.getElementById('setClass').addEventListener('click',addClass);
+
+// task no05
+removeClass = () => {
+    let targetDiv = document.getElementById('hasClass');
+    targetDiv.classList.remove('newClass');
+}
+document.getElementById('hasClass').addEventListener('click',removeClass);
