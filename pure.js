de0fc134447ee5545122437ce1e3dc9d08a1d974
n-nakajm <n-nakajm@ist.osaka-u.ac.jp>
2018-07-24 20:44:11 +0900

    make task04

--- pure.js
+++ pure.js
@@ -1,15 +1,27 @@
 // task01
-function changeTextColor(){
+changeTextColor = () => {
     let text = document.getElementById('targetOfChangeColor');
     text.style.color = '#ff95c8';
 }
-document.getElementById('TextColor').addEventListener('click',changeTextColor);
+document.getElementById('TextColor').addEventListener('click', changeTextColor);
 
 // task02
-function changeText(){
+changeText = () => {
     let text = document.getElementById('targetOfChangeText');
     text.innerHTML = 'Hello,JavaScript!';
 }
-document.getElementById('firstText').addEventListener('click',changeText);
+document.getElementById('firstText').addEventListener('click', changeText);
 
 // task03
+showTypedText = () => {
+    let typed = document.getElementById('typeText').value;
+    document.getElementById('typedText').innerHTML = typed;
+}
+document.getElementById('typeText').addEventListener('keyup',showTypedText);
+
+// task04
+addDifferentClass = () => {
+    let targetDiv = document.getElementById('setClass');
+    targetDiv.classList.add('newClass');
+}
+document.getElementById('setClass').addEventListener('click',addDifferentClass);
