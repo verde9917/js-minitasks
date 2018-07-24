1a30804980fd5d58896bf93dec825e1c1f8f71a0
n-nakajm <n-nakajm@ist.osaka-u.ac.jp>
2018-07-24 21:41:08 +0900

    make task07

--- pure.js
+++ pure.js
@@ -1,40 +1,59 @@
 // task no01
-changeTextColor = () => {
+let changeTextColor = () => {
     let text = document.getElementById('targetOfChangeColor');
     text.style.color = '#ff95c8';
-}
+};
 document.getElementById('TextColor').addEventListener('click', changeTextColor);
 
 // task no02
-changeText = () => {
+let changeText = () => {
     let text = document.getElementById('targetOfChangeText');
     text.innerHTML = 'Hello,JavaScript!';
-}
+};
 document.getElementById('firstText').addEventListener('click', changeText);
 
 // task no03
-showTypedText = () => {
+let showTypedText = () => {
     document.getElementById('typedText').innerHTML = document.getElementById('typeText').value;
-}
+};
 document.getElementById('typeText').addEventListener('keyup',showTypedText);
 
 // task no04
-addClass = () => {
+let addClass = () => {
     let targetDiv = document.getElementById('addClass');
     targetDiv.classList.add('newClass');
-}
+};
 document.getElementById('addClass').addEventListener('click',addClass);
 
 // task no05
-removeClass = () => {
+let removeClass = () => {
     let targetDiv = document.getElementById('removeClass');
     targetDiv.classList.remove('newClass');
-}
+};
 document.getElementById('removeClass').addEventListener('click',removeClass);
 
 // task no06
-toggleClass = () => {
+let toggleClass = () => {
     let targetDiv = document.getElementById('toggleClass');
     targetDiv.classList.toggle('newClass');
-}
+};
 document.getElementById('toggleClass').addEventListener('click',toggleClass);
+
+// task no07
+// 参考 : https://q-az.net/without-jquery-fadein-fadeout-slideup-slidedown/
+let fadeIn = () => {
+    let begin = new Date();
+    let target = document.getElementById('fadeInText');
+    let Time = 1000;
+    let id = setInterval(() => {
+        let current = new Date() - begin;
+        if (current > Time){
+            clearInterval(id);
+            current = Time;
+        }
+        target.style.opacity = current / Time;
+    }, 10);
+};
+document.getElementById('fadeInButton').addEventListener('click',fadeIn);
+
+// task 08
